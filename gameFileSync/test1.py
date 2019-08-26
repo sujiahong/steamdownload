2bd57db06481ddd6524723b0d5a6e4ce8e64fd15
sujiahong <1511877962@qq.com>
2019-08-26 09:16:14 +0800

    修改

--- gameFileSync/test1.py
+++ gameFileSync/test1.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
-# os.system("D:/soft/cwRsync/bin/rsync.exe -aczr --delete --progress /cygdrive/f/test /cygdrive/e")
+ret = os.system("D:/soft/cwRsync/bin/rsync.exe -aczr --delete --progress /cygdrive/f/test /cygdrive/e")
 #subprocess.call("D:\soft\cwRsync\\bin\\rsync.exe -aczr --delete --progress /cygdrive/f/test /cygdrive/e")
 
+print(ret)
